ab110d6e7db2a9653b3297bafe7241a2126e43e6
Barry Wang <36621849+Barrywww@users.noreply.github.com>
2021-04-25 16:31:47 +0800

    demo commit

--- server.py
+++ server.py
@@ -18,21 +18,54 @@ us2us = {}
 def home():
     return render_template("index.html")
 
+@app.route('/get_user_list/<string:username>')
+def get_user_list(username):
+    ul = []
+    for user in users:
+        if user != username:
+            ul.append(user)
+    ud = {"list":ul} #result.list is the user list, same below
+    return json.dumps(ud, ensure_ascii=False)
+
+@app.route('/login/<string:username>')
+def login(username):
+    if username in users:
+        ud = {"ans": "Unsuccess"}
+    else:
+        ud = {"ans": "Success"}
+    return json.dumps(ud)
+
+@app.route('/get_group_list')
+def get_group_list():
+    ud = {"list":list(groups.keys())}
+    return json.dumps(ud, ensure_ascii=False)
+
+@app.route('/logout/<string:username>', methods=["GET"])
+def logout(username):
+    if username not in users:
+        return redirect('/')
+    print("User '%s' logged out." % username)
+    users.remove(username)
+    usn2id.pop(username, None)
+    us2us.pop(username, None)
+    us2grp.pop(username, None)
 
 @socket.on('message')
 def handle_message(msg):
     msg = json.loads(msg)
     print("Received message")
     print(msg)
+    print(users)
     if msg["type"] == "connect":
         user_id = msg["id"]
         username = msg["usn"]
         usn2id[username] = user_id
 
+        users.append(username)
         msg2c = username + " has joined in the chat!"
         for member in groups["Broadcast"]:
             if (member != username) and (us2grp[member] == "Broadcast"):
-                send(json.dumps(msg2c), rec=usn2id[member])
+                send(json.dumps(msg2c), to=usn2id[member])
         if username not in groups['Broadcast']:
             groups["Broadcast"].append(username)
         us2grp[username] = "Broadcast"
@@ -45,19 +78,22 @@ def handle_message(msg):
         target = msg["tgt"]
 
         msg2c = source + ": " + content
-        if msg["sendType"] == "public":
+        if msg["sendType"] == "group":
+            print(groups[target])
+            print(msg)
             for member in groups[target]:
-                if (member != source) and (us2grp[member] == target):
-                    send(json.dumps(msg2c), rec=usn2id[member])
+                if (us2grp[member] == target):
+                    send(json.dumps(msg2c), to=usn2id[member])
         elif msg["sendType"] == "private":
-            if us2us[target] == source:
-                send(json.dumps(msg2c), rec=usn2id[target])
-            elif target in users:
-                msg2c = target + "  is now in another chat. Please try again later."
-                send(json.dumps(msg2c), rec=usn2id[source])
+            if target not in users:
+                msg2c = target + " is now offline."
+                send(json.dumps(msg2c), to=usn2id[source])
+            elif us2us[target] == source:
+                send(json.dumps(msg2c), to=usn2id[target])
+                send(json.dumps(msg2c), to=usn2id[source])
             else:
-                msg2c = target + "  is now offline."
-                send(json.dumps(msg2c), rec=usn2id[source])
+                msg2c = target + " is now in another chat. Please try again later."
+                send(json.dumps(msg2c), to=usn2id[source])
 
         print("'%s' sent a message '%s' to '%s'." % (source, content, target))
 
@@ -66,37 +102,57 @@ def handle_message(msg):
         target = msg["tgt"]
 
         if us2us[username]:
-            print("'%s' stopped receiving message from '%s'." % (username, target))
-            msg2c = username + "has left the chat."
+            print("'%s' stopped receiving message from '%s'." % (username, us2us[username]))
+            msg2c = username + " has left the chat."
             if us2us[us2us[username]] == username:
-                send(json.dumps(msg2c), rec=usn2id[us2us[username]])
+                send(json.dumps(msg2c), to=usn2id[us2us[username]])
             us2us[username] = ''
         elif us2grp[username]:
-            print("'%s' stopped receiving message from '%s'." % (username, target))
-            msg2c = username + "has left the group."
+            print("'%s' stopped receiving message from '%s'." % (username, us2grp[username]))
+            msg2c = username + " has left the "+us2grp[username]+" group."
             for member in groups[us2grp[username]]:
                 if (member != username) and (us2grp[member] == us2grp[username]):
-                    send(json.dumps(msg2c), rec=usn2id[member])
+                    send(json.dumps(msg2c), to=usn2id[member])
+            groups[us2grp[username]].remove(username)
             us2grp[username] = ''
 
         if msg["joinType"] == "private":
             us2us[username] = target
-            msg2c = username + "has entered the chat."
             if us2us[target] == username:
-                send(json.dumps(msg2c), rec=usn2id[target])
+                msg2c = username + " has entered the chat."
+                send(json.dumps(msg2c), to=usn2id[target])
+            else:
+                msg2c = "$i" + username + " has initiated a chat with you!"
+                send(json.dumps(msg2c), to=usn2id[target])
         elif msg["joinType"] == "group":
             us2grp[username] = target
-            msg2c = username + "has entered the group."
+            if username not in groups[target]:
+                groups[target].append(username);
+            msg2c = username + " has entered the group."
             for member in groups[us2grp[username]]:
-                if (member != username) and (us2grp[member] == target):
-                    send(json.dumps(msg2c), rec=usn2id[member])
+                if (us2grp[member] == target):
+                    send(json.dumps(msg2c), to=usn2id[member])
 
         print("'%s' joined the chat with '%s'." % (username, target))
 
     elif msg["type"] == "create":
         username = msg["usn"]
         groupname = msg["grpnm"]
-        groups[groupname] = [username]
+        groups[groupname] = []
+
+        if us2us[username]:
+            print("'%s' stopped receiving message from '%s'." % (username, us2us[username]))
+            msg2c = username + " has left the chat."
+            if us2us[us2us[username]] == username:
+                send(json.dumps(msg2c), to=usn2id[us2us[username]])
+            us2us[username] = ''
+        elif us2grp[username]:
+            print("'%s' stopped receiving message from '%s'." % (username, us2grp[username]))
+            msg2c = username + " has left the "+us2grp[username]+" group."
+            for member in groups[us2grp[username]]:
+                if (member != username) and (us2grp[member] == us2grp[username]):
+                    send(json.dumps(msg2c), to=usn2id[member])
+            us2grp[username] = ''
 
         print("'%s' created group '%s'." % (username, groupname))
 
@@ -104,11 +160,13 @@ def handle_message(msg):
         username = msg["usn"]
         if username not in users:
             return redirect('/')
-        print("User '%s' logs out." % username)
+        print("User '%s' logged out." % username)
         users.remove(username)
+        groups[us2grp[username]].remove(username)
         usn2id.pop(username, None)
         us2us.pop(username, None)
         us2grp.pop(username, None)
+        
 
 
 if __name__ == "__main__":
--- static/js/index.js
+++ static/js/index.js
@@ -1,37 +1,44 @@
-let socket = {
-    run: function(){
-        console.log("init socket");
-        this.socket = new WebSocket(SOCKET_SERVER_ADDR);
-        this.socket.onopen = this.onOpen;
-        this.socket.onerror = this.onErr;
-        this.socket.onmessage = this.handleMessage;
-    },
-
-    onOpen: function(){
-        console.log("Socket connection success");
-    },
-
-    onErr: function(event){
-        alert("Socket Failure");
-
-    },
-
-    handleMessage: function(e){
-        let msg = JSON.parse(e.data);
-        console.log(msg);
-    }
-};
-
-let SERVER_ADDR = "http://127.0.0.1:5500";
-let SOCKET_SERVER_ADDR = "ws://127.0.0.1:6060"
-let userList = [{"username": "Leon", "uid": 1}, {"username": "Barry", "uid": "2"}];
-let groupList = [{"groupName": "Barry, Leon", "gid": "2", "users": "2"}];
+let socket;
+let username;
+let chatInput;
+let msgBox;
+let grpn = "Broadcast";
+let SERVER_ADDR = "http://127.0.0.1:6060";
 let currentActive;
+let currentChatType = "group";
+let currentSelectorType = "group";
+
+const initSocket = function (){
+    return new Promise((resolve, reject) => {
+        socket = io('http://127.0.0.1:6060');
+        socket.on('connect', () =>{
+            console.log("connected to socket");
+            socket.send(JSON.stringify({"id": socket.id, "usn": username, "type":"connect"}))
+            resolve();
+        });
+
+        socket.on("message", (msg) => {
+                msg = msg.slice(1,-1);
+                if (msg.slice(0,2) !== "$i"){
+                    displayMessage(msg);
+                }
+                else{
+                    displayMessage(msg.slice(2,-1));
+                }
+            }
+        );
+    })
+};
 
 window.onload = function () {
     console.log("script loaded");
     document.getElementById("loginButton").addEventListener("click", loginCheck);
 }
+
+window.onunload = async function (e){
+    await logout();
+}
+
 
 function loginCheck(){
     let username = document.getElementById("indexLoginInput").value;
@@ -42,142 +49,284 @@ function loginCheck(){
     progress.setAttribute("class", progress.getAttribute("class").replace("danger", "success"))
 
     console.log("loading");
-    // socket.run();
 
-    const response = function(){return fetch(SERVER_ADDR + "/login", {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({"username": username}),
-    })}
+    const response = function(){
+        try{
+            return fetch(SERVER_ADDR + "/login/" + username, {
+            method: "GET",
+            headers: { 'Content-Type': 'application/json' }})}
+        catch(e){return new Promise.reject();}
+    }
 
-    response().then((r) => {
-        if (r.status === 200){
-            console.log("s");
-            setTimeout(onLogin, 500);
+    response().then((r) => r.json(), () => {
+        setTimeout(() => {
+            onLoginFail("Server failure! Please check server status or restart.");
+        }, 500);
+    }).then((r) => {
+        if (r["ans"] === "Success"){
+            onLogin();
         }
         else{
-            alert("Username Existed! Please try again.");
-            onLoginFail();
+            setTimeout(() => {
+                onLoginFail("Username existed! Please try another one.");
+            }, 500);
+
+
         }
     });
 
-    for(let i=1; i<=90; i++){
+    for(let i=1; i<=20; i++){
         setTimeout(() => {
             console.log(i);
-            progress.style.setProperty("width", i + "%")}, 10*i);
+            if (i >= parseFloat(progress.style.width)){
+            progress.style.setProperty("width", i + "%")}}, 10*i);
     }
 }
 
 function onLogin(){
-    let username = document.getElementById("indexLoginInput").value;
-    localStorage.setItem("username", username);
-    localStorage.setItem("loggedIn", "true");
-
+    username = document.getElementById("indexLoginInput").value;
+    chatInput = document.getElementById("msgInputInner");
+    msgBox = document.getElementById("msgBoxInner");
     let progress = document.getElementById("loginProgress");
-    for(let i=91; i<=100; i++){
-        setTimeout(() => {
-            console.log(i);
-            progress.style.setProperty("width", i + "%")}, 3*i);
-    }
-
     let loginPanel = document.getElementById("loginWrapper");
     let chatPanel = document.getElementById("chatWrapper");
-    let msgBox = document.getElementById("msgBoxInner");
-    document.getElementById("buttonUser").addEventListener("click", (e) =>{
-        refreshUsers();
-        e.target.parentNode.setAttribute("class", "active selector");
-        let g = document.getElementById("buttonGroup");
-        g.setAttribute("class", "selector");
-        e.preventDefault();
-        e.stopPropagation();
-    })
-    document.getElementById("buttonGroup").addEventListener("click", (e) => {
+    let greetings = document.getElementById("loginGreetings");
+    let cGrettings = document.getElementById("chatGreeting");
+
+    initSocket().then(() => {
+        localStorage.setItem("username", username);
+        localStorage.setItem("loggedIn", "true");
+        greetings.innerHTML = "<h1>Welcome, " + username + "</h1>";
+        cGrettings.innerHTML = "<h1>Welcome, " + username + "</h1>";
+        for(let i=21; i<=100; i++){
+            setTimeout(() => {
+                console.log(i);
+                progress.style.setProperty("width", i + "%")}, 1*i);
+            if (i === 100){
+                setTimeout(() => {
+                    loginPanel.style.display = "none";
+                    chatPanel.style.display = "flex";
+                }, 1000);
+            }
+        }
+
+        document.getElementById("buttonUser").addEventListener("click", (e) =>{
+            currentSelectorType = "user";
+            refreshUsers();
+            e.target.parentNode.setAttribute("class", "active selector");
+            let g = document.getElementById("buttonGroup");
+            g.setAttribute("class", "selector");
+            e.preventDefault();
+            e.stopPropagation();
+        })
+        document.getElementById("buttonGroup").addEventListener("click", (e) => {
+            currentSelectorType = "group";
+            refreshGroups();
+            e.target.parentNode.setAttribute("class", "active selector");
+            let u = document.getElementById("buttonUser");
+            u.setAttribute("class", "selector");
+            e.preventDefault();
+            e.stopPropagation();
+        })
+
+        document.getElementById("logoutButton").addEventListener("click", logout);
+        document.getElementById("sendButton").addEventListener("click", sendMsg);
+
         refreshGroups();
-        e.target.parentNode.setAttribute("class", "active selector");
-        let u = document.getElementById("buttonUser");
-        u.setAttribute("class", "selector");
-        e.preventDefault();
-        e.stopPropagation();
-    }, true)
-
-    document.getElementById("logoutButton").addEventListener("click", logout);
-    refreshUsers();
-
-
-    setTimeout(() => {
-        loginPanel.style.display = "none";
-        chatPanel.style.display = "flex";
-    }, 1000);
+        setInterval(() => {
+            if (currentSelectorType === "group"){
+                refreshGroups();
+            }
+            else{
+                refreshUsers();
+            }
+        }, 5000);
+    })
 }
 
-function onLoginFail(){
+function onLoginFail(msg){
     let progress = document.getElementById("loginProgress");
     progress.setAttribute("class", progress.getAttribute("class").replace("success", "danger"))
+    alert(msg);
 }
 
 function refreshUsers(){
-    const response = function () {return fetch(SERVER_ADDR + "/get_user_list", {method: "GET"})}
-    let userLst = document.getElementById("user-group-ul");
+    const response = function () {return fetch(SERVER_ADDR + "/get_user_list/" + username, {method: "GET"})}
+    let userLst = document.getElementById("user-ul");
+    let groupLst = document.getElementById("group-ul");
     userLst.innerHTML = '';
-    response().then(response => response.json()).then(json => {
-        for (let n of json["users"]){
+    response().then(response => response.text()).then(json => {
+        json = JSON.parse(json);
+        console.log(json);
+        for (let n of json["list"]){
             let user = document.createElement("li");
-            user.setAttribute("class", "list-group-item chat-user-group");
-            user.addEventListener("click", function (){
-                if (currentActive !== undefined){
-                    currentActive.setAttribute("class",
-                        currentActive.getAttribute("class")
-                            .replace(" active", ""))
-                }
-                this.setAttribute("class", "list-group-item chat-user-group active");
+            if (currentActive !== undefined && n === currentActive.innerText && currentChatType === "private"){
+                user.setAttribute("class", "list-group-item chat-user-group active");
                 currentActive = user;
+            }
+            else{
+                user.setAttribute("class", "list-group-item chat-user-group");
+            }
+            user.addEventListener("click", function (){
+                if (this !== currentActive){
+                    if (currentActive !== undefined){
+                        currentActive.setAttribute("class",
+                            currentActive.getAttribute("class")
+                                .replace(" active", ""))
+                    }
+                    this.setAttribute("class", "list-group-item chat-user-group active");
+                    currentActive = user;
+                    msgBox.innerHTML = "";
+                    currentChatType = "private";
+                    joinPrivateChat(n);
+                }
             })
-            user.innerText = n.username;
+            user.innerText = n;
             userLst.appendChild(user);
         }
+        userLst.style.display = "";
+        groupLst.style.display = "none";
     })
 }
 
 function refreshGroups(){
     const response = function () {return fetch(SERVER_ADDR + "/get_group_list", {method: "GET"})}
-    let groupLst = document.getElementById("user-group-ul");
-    groupLst.innerHTML = "";
-    response().then(response => response.json()).then(json => {
-        for (let n of json["groups"]){
+    let userLst = document.getElementById("user-ul");
+    let groupLst = document.getElementById("group-ul");
+    let ch = groupLst.childNodes;
+    let more_update = true;
+    for(let i = ch.length - 1; i >= 1; i--) {
+        groupLst.removeChild(ch[i]);
+    }
+    document.getElementById("newGrp").addEventListener("click", newGroup);
+    response().then(response => response.text()).then(json => {
+        json = JSON.parse(json);
+        for (let n of json["list"]){
             let group = document.createElement("li");
-            group.setAttribute("class", "list-group-item chat-user-group");
-            group.innerText = n.groupName;
-            let badge = document.createElement("span");
-            badge.setAttribute("class", "badge");
-            badge.innerHTML = n.users;
-            group.appendChild(badge);
-            group.addEventListener("click", function (){
-                if (currentActive !== undefined){
-                    currentActive.setAttribute("class",
-                        currentActive.getAttribute("class")
-                            .replace(" active", ""))
-                }
-                this.setAttribute("class", "list-group-item chat-user-group active");
+            if (n === grpn && currentChatType === "group"){
+                more_update = false;
+                console.log(grpn + "set");
+                group.setAttribute("class", "list-group-item chat-user-group active");
                 currentActive = group;
+                let children = groupLst.children
+                for (let c=1; c<children.length;c++){
+                    children[c].setAttribute("class", "list-group-item chat-user-group");
+                }
+
+            }
+            else if (more_update && currentActive !== undefined && n === currentActive.innerText && currentChatType === "group"){
+                group.setAttribute("class", "list-group-item chat-user-group active");
+                currentActive = group;
+            }
+            else{
+                group.setAttribute("class", "list-group-item chat-user-group");
+            }
+            group.innerText = n;
+            // let badge = document.createElement("span");
+            // badge.setAttribute("class", "badge");
+            // badge.innerHTML = n.users;
+            // group.appendChild(badge);
+            group.addEventListener("click", function (){
+                if (currentActive !== this){
+                    if (currentActive !== undefined){
+                        currentActive.setAttribute("class",
+                            currentActive.getAttribute("class")
+                                .replace(" active", ""))
+                    }
+                    this.setAttribute("class", "list-group-item chat-user-group active");
+                    currentActive = group;
+                    currentChatType = "group";
+                    msgBox.innerHTML = "";
+                    joinGroupChat(n);
+                }
             })
             groupLst.appendChild(group);
         }
+        groupLst.style.display = "";
+        userLst.style.display = "none";
     })
 }
 
+function joinPrivateChat(user){
+    let msg = {
+        "usn": username,
+        "tgt": user,
+        "type": "join",
+        "content": " joined the chat.",
+        "joinType": "private"
+    }
+    socket.send(JSON.stringify(msg));
+}
+
+function newGroup(){
+    let grpName = document.getElementById("grpName")
+    let msg = {
+        "usn": username,
+        "type": "create",
+        "grpnm": grpName.value
+    }
+    let value = grpName.value;
+    grpn = value;
+    socket.send(JSON.stringify(msg));
+    const doUpdate =  () => {
+        refreshGroups();
+        joinGroupChat(value);
+    }
+    doUpdate();
+    grpName.value = "";
+}
+
+function joinGroupChat(grpName){
+    let msg = {
+        "usn": username,
+        "tgt": grpName,
+        "type": "join",
+        "content": " joined the chat.",
+        "joinType": "group"
+    }
+    socket.send(JSON.stringify(msg));
+}
+
+function sendMsg(){
+    let msg = chatInput.value;
+    chatInput.value = "";
+    let tgt = currentActive.innerText;
+    let m = {
+        "src": username,
+        "tgt": tgt,
+        "type": "msg",
+        "content": msg,
+        "sendType": currentChatType
+    }
+    socket.send(JSON.stringify(m));
+}
+
+function displayMessage(msg){
+    console.log(msg);
+    msgBox.innerHTML += msg + "<br><br>";
+    msgBox.scrollTop = msgBox.scrollHeight;
+    if (currentSelectorType === "group"){
+        refreshGroups();
+    }
+    else{
+        refreshUsers();
+    }
+}
+
 async function logout(){
-    const result = await socket.socket.close();
-    const response = function () {return fetch(SERVER_ADDR + "/logout", {
-        method: "POST",
-        body: JSON.stringify({
-            username: localStorage.getItem("username")
-        })
-    })}
-    response().then(response => response.json()).then(json => {
-        if (json["result"] === "success"){
+    if (socket !== undefined){
+        socket.send(JSON.stringify({
+            "usn": username,
+            "type": "logout"
+        }))
+        const result = await socket.close();
+        const response = function () {return fetch(SERVER_ADDR + "/logout/" + username, {
+            method: "GET",
+        })}
+        response().then(() => {
             localStorage.removeItem("username");
             localStorage.removeItem("loggedIn");
             location.reload();
-        }
-    })
+        });
+    }
 }
